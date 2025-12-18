7cf238c6a79dd28b419f3117d13af98d32d8324f
Sky11y <jyri.piensalo@gmail.com>
2025-12-18 15:01:47 +0200

    Moved string related functions to new file strings.c. Updated function descriptions in todo.h

---
+++ strings.c
@@ -0,0 +1,141 @@
+#include "todo.h"
+
+void strtrim(char *restrict str, const char* restrict chars)
+{
+	uint i = 0;
+	uint j = strlen(str);
+
+	//empty string -> return
+	if (i == j) {
+		return ;
+	}
+
+	while (i < j && is_any_of(str[i], chars) == 1) {
+		++i;
+	}
+
+	//contains only *chars*. Return empty string.
+	if (i == j) {
+		str[0] = '\0';
+		return ;
+	}
+
+	while (j > i && is_any_of(str[j - 1], chars) == 1) {
+		--j;
+	}
+
+	memmove(str, &str[i], j - i);
+	str[j - i] = '\0';
+}
+
+void strtrim_front(char *restrict str, const char *restrict chars)
+{
+	uint i = 0;
+	uint len;
+
+	while (str[i] && is_any_of(str[i], chars) == 1) {
+		++i;
+	}
+
+	if (str[i] == '\0') {
+		str[0] = '\0';
+		return ;
+	}
+
+	len = strlen(str) - i;
+	memmove(str, &str[i], len);
+	str[len] = '\0';
+}
+
+void strtrim_back(char *restrict str, const char *restrict chars)
+{
+	uint i = strlen(str);
+
+	if (i == 0) {
+		return ;
+	}
+
+	while (i > 0 && is_any_of(str[i - 1], chars) == 1) {
+		--i;
+	}
+
+	str[i] = '\0';
+}
+
+uint safe_strlcpy(char *restrict dst, const char *restrict src, uint dsize)
+{
+	uint src_len = 0;
+	uint i;
+
+	while (src[src_len] && src_len < dsize - 1) {
+		++src_len;
+	}
+
+	for (i = 0; i < src_len; ++i) {
+		dst[i] = src[i];
+	}
+
+	dst[src_len] = '\0';
+	return src_len;
+}
+
+int contains(const char *restrict test, const char *restrict chars)
+{
+	uint i;
+
+	if (test == NULL) {
+		return -1;
+	}
+
+	if (chars == NULL) {
+		return 1;
+	}
+
+	for (i = 0; test[i]; ++i) {
+		if (is_any_of(test[i], chars) == 1) {
+				return 1;
+		}
+	}
+	return 0;
+}
+
+int is_any_of(const char test, const char *restrict chars)
+{ 
+	uint i;
+
+	if (test == '\0') {
+		return -1;
+	}
+
+	if (chars == NULL) {
+		return 1;
+	}
+
+	for (i = 0; chars[i]; ++i) {
+		if (test == chars[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int is_all_of(const char *restrict test, const char *restrict chars)
+{
+	uint i;
+	
+	if (!test) {
+		return -1;
+	}
+
+	if (!chars) {
+		return 1;
+	}
+
+	for (i = 0; test[i]; ++i) {
+		if (is_any_of(test[i], chars) == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
--- todo.h
+++ todo.h
@@ -49,30 +49,32 @@ int read_datafile(int fd, s_data* data);
  * Copies max. *dsize* - 1 chars from *src* to *dst*.
  * Guarantees to '\0'-terminate the *dst* (hence max. dsize - 1).
  * Returns the number of characters copied, exluding the terminating '\0'.
+ * Note. If *src* is empty -> *dst* will be empty.
+ * Note. It is users responsiblity to make sure the size of *dst* is large enough.
  */
 uint safe_strlcpy(char *restrict dst, const char *restrict src, uint dsize);
 
 /*
  * Checks if string *test* contains any of the characters in *chars*.
  * Returns 1 if any of the *chars* is found in *test* and 0 if not.
- * If *test* is empty returns -1 (user can handle this as an error).
- * If *chars* is empty returns 1 (i.e. always true when compared to nothing).
+ * Note. If *test* is empty returns -1 (user can handle this as an error).
+ * Note. If *chars* is empty returns 1 (i.e. always true when compared to nothing).
  */
 int contains(const char *restrict test, const char *restrict chars);
 
 /*
  * Checks if a character *test* is any of the character in *chars*.
  * Returns 1 if test matches any of the *chars*, and 0 if not.
- * If *test* is null-character returns -1 (user can handle this as an error).
- * If *chars* is empty returns 1 (i.e. always true when compared to nothing).
+ * Note. If *test* is null-character returns -1 (user can handle this as an error).
+ * Note. If *chars* is empty returns 1 (i.e. always true when compared to nothing).
  */
 int is_any_of(const char test, const char *restrict chars);
 
 /*
  * Check if all the characters in *test* are one of *chars*
  * Returns 1 if true, and 0 if not.
- * If *test* is empty returns -1 (user can handle this as an error).
- * If *chars* is empty returs 1 (i.e. always true when compared to nothing).
+ * Note. If *test* is empty returns -1 (user can handle this as an error).
+ * Note. If *chars* is empty returs 1 (i.e. always true when compared to nothing).
  */
 int is_all_of(const char *restrict test, const char *restrict chars);
 
